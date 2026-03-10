672db7fe3eb8cb61868c4779fe20aee55a454c99
dhd1003 <119531097+dhd1003@users.noreply.github.com>
2026-03-10 21:59:22 +0100

    Subir archivos a GIT

---
+++ Practica1/Practica1/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Practica1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Practica1/Practica1/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Practica1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+
+}
