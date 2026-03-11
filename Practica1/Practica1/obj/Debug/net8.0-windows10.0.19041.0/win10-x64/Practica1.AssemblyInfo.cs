02f203cb194dd7b69d32656122a6b2c5998d6eba
dhd1003 <119531097+dhd1003@users.noreply.github.com>
2026-03-11 01:35:59 +0100

    Commit2

--- Practica1/Practica1/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Practica1.AssemblyInfo.cs
+++ Practica1/Practica1/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Practica1.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practica1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+672db7fe3eb8cb61868c4779fe20aee55a454c99")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practica1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practica1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
